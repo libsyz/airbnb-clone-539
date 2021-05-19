61aa28b37021dc18e19959880f08c898219d9380
miguel jimenez <denkatalabeno@gmail.com>
2021-05-20 00:11:59 +0800

    created routes, controller action and view for the apartments show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'apartments/index'
   devise_for :users
   root to: 'pages#home'
-  resources :apartments, only: [:index]
+  resources :apartments, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
